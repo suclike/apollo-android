9ace43b1e33dc0f3500cb877924ac1914bff9c12
Marwan <mrwnad@gmail.com>
2017-05-19 20:47:04 -0400

    refactor compiler write method

--- apollo-compiler/src/main/kotlin/com/apollographql/android/compiler/GraphQLCompiler.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/android/compiler/GraphQLCompiler.kt
@@ -6,52 +6,75 @@ import com.apollographql.android.compiler.ir.TypeDeclaration
 import com.squareup.javapoet.JavaFile
 import com.squareup.moshi.Moshi
 import java.io.File
+import java.util.ArrayList
 
 class GraphQLCompiler {
   private val moshi = Moshi.Builder().build()
   private val irAdapter = moshi.adapter(CodeGenerationIR::class.java)
 
-  fun write(args: Arguments) {
-    val ir = irAdapter.fromJson(args.irFile.readText())
-    val irPackageName = args.irFile.absolutePath.formatPackageName()
+  fun write(args: Arguments): List<File> {
+    val codeGenContext = args.codeGenerationContext()
+    val javaFiles = codeGenContext.ir.toJavaFiles(codeGenContext)
+    javaFiles.write(args.outputDir)
+    return javaFiles.outputFiles(args.outputDir)
+  }
+
+  fun List<JavaFile>.outputFiles(outputDir: File): List<File> {
+    val outputFiles: MutableList<File> = ArrayList()
+    forEach {
+      var outputDirectory = outputDir
+      it.packageName.split("\\.").forEach {
+        outputDirectory = outputDirectory.resolve(it)
+      }
+      outputFiles.add(File(outputDirectory, "${it.typeSpec.name}.java"))
+    }
+    return outputFiles
+  }
+
+  private fun Arguments.codeGenerationContext(): CodeGenerationContext {
+    val ir = irAdapter.fromJson(irFile.readText())
+    val irPackageName = irFile.absolutePath.formatPackageName()
     val fragmentsPackage = if (irPackageName.isNotEmpty()) "$irPackageName.fragment" else "fragment"
     val typesPackage = if (irPackageName.isNotEmpty()) "$irPackageName.type" else "type"
-    val supportedScalarTypeMapping = args.customTypeMap.supportedScalarTypeMapping(ir.typesUsed)
-    val context = CodeGenerationContext(
+    val supportedScalarTypeMapping = customTypeMap.supportedScalarTypeMapping(ir.typesUsed)
+
+    return CodeGenerationContext(
         reservedTypeNames = emptyList(),
         typeDeclarations = ir.typesUsed,
         fragmentsPackage = fragmentsPackage,
         typesPackage = typesPackage,
         customTypeMap = supportedScalarTypeMapping,
-        nullableValueType = args.nullableValueType,
-        generateAccessors = args.generateAccessors,
+        nullableValueType = nullableValueType,
+        generateAccessors = generateAccessors,
         ir = ir)
-    ir.writeJavaFiles(context, args.outputDir)
   }
 
-  private fun CodeGenerationIR.writeJavaFiles(context: CodeGenerationContext, outputDir: File) {
+  private fun CodeGenerationIR.toJavaFiles(context: CodeGenerationContext): List<JavaFile> {
+    val javaFiles: MutableList<JavaFile> = ArrayList()
     fragments.forEach {
       val typeSpec = it.toTypeSpec(context.copy())
-      JavaFile.builder(context.fragmentsPackage, typeSpec).build().writeTo(outputDir)
+      javaFiles.add(JavaFile.builder(context.fragmentsPackage, typeSpec).build())
     }
 
     typesUsed.supportedTypeDeclarations().forEach {
       val typeSpec = it.toTypeSpec(context.copy())
-      JavaFile.builder(context.typesPackage, typeSpec).build().writeTo(outputDir)
+      javaFiles.add(JavaFile.builder(context.typesPackage, typeSpec).build())
     }
-
     if (context.customTypeMap.isNotEmpty()) {
       val typeSpec = CustomEnumTypeSpecBuilder(context.copy()).build()
-      JavaFile.builder(context.typesPackage, typeSpec).build().writeTo(outputDir)
+      javaFiles.add(JavaFile.builder(context.typesPackage, typeSpec).build())
     }
 
     operations.map { OperationTypeSpecBuilder(it, fragments) }
         .forEach {
           val packageName = it.operation.filePath.formatPackageName()
           val typeSpec = it.toTypeSpec(context.copy())
-          JavaFile.builder(packageName, typeSpec).build().writeTo(outputDir)
+          javaFiles.add(JavaFile.builder(packageName, typeSpec).build())
         }
+    return javaFiles
   }
+
+  private fun List<JavaFile>.write(outputDir: File) = forEach { it.writeTo(outputDir) }
 
   private fun List<TypeDeclaration>.supportedTypeDeclarations() =
       filter { it.kind == TypeDeclaration.KIND_ENUM || it.kind == TypeDeclaration.KIND_INPUT_OBJECT_TYPE }
